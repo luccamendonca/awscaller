eb02ea7e6342a55445641ed157ec67262d6eec10
Lucca Mendonça <lucca.mendonca@printi.com.br>
2018-03-14 13:07:39 -0300

    Added support to query string
    Now we can build simple query strings that will be applied to the URL.
    
    Usage:
    
    aws <service> <key>:<value>...
    
    Example:
    
    aws sqs region:us-east-1

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@
 
 from albertv0 import *
 import os
+import re
+from urllib.parse import urlencode
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = 'Aws Caller'
@@ -11,14 +13,35 @@ __author__ = '<Lucca Mendonça> lucca.mendonca@gmail.com'
 __dependencies__ = []
 __icon_path__ = os.path.dirname(__file__)+"/logo.svg"
 
+def getServiceAndParams(query_string):
+    query = re.split('\s+', query_string)
+    response = {"service_name": query.pop(0),
+                "query_string": ""}
+
+    if len(query) == 0:
+        return response
+
+    query_params = {}
+
+    for item in query:
+        if ':' not in item:
+            continue
+        param=re.split(':', item)
+        if len(param[1]) == 0 or len(param[0]) == 0:
+            continue
+        query_params[param[0]] = param[1]
+
+    if len(query_params) > 0:
+        response["query_string"] = "?"+urlencode(query_params)
+
+    return response
+
 def handleQuery(query):
     if not query.isTriggered:
         return
 
-    service_name = query.string.strip()
-
-    aws_url = "https://console.aws.amazon.com/" + service_name
-
+    params = getServiceAndParams(query.string.strip())
+    aws_url = "https://console.aws.amazon.com/" + params["service_name"] + params["query_string"]
     item = Item(id=__prettyname__,
                 icon=__icon_path__,
                 completion=query.rawString,
